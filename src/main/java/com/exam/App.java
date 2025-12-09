0042fe34c3027f3e959543e0cbac58b96d5278e3
dhanushmr0211 <1si24cs051>
2025-12-09 23:57:54 +0530

    testing auto_build_5

--- src/main/java/com/exam/App.java
+++ src/main/java/com/exam/App.java
@@ -2,6 +2,6 @@ package com.exam;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Auto Build Trigger Test 4!");
+        System.out.println("Auto Build Trigger Test 5!");
     }
 }
